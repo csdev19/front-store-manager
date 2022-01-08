c594ddedec13e7c1421487d90e6808c1183ed65e
cristiandev <cristiansotomayor.dev@gmail.com>
2022-01-08 18:51:48 -0500

    new category interface

---
+++ core/interfaces/categories.interface.ts
@@ -0,0 +1,11 @@
+export interface ICategory {
+  id: number;
+  name: string;
+  description: string;
+}
+
+export interface ICategoryCreate {
+  id: number;
+  name: string;
+  description: string;
+}
