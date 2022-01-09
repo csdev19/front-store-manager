2cfdb660037b86b9ef43b693400ff75d854684d4
cristiandev <cristiansotomayor.dev@gmail.com>
2022-01-09 00:53:04 -0500

    login page

---
+++ pages/authentication/login.tsx
@@ -0,0 +1,9 @@
+import LoginLayout from 'modules/authentication/LoginLayout';
+import { NextPage } from 'next';
+import React from 'react';
+
+const login: NextPage = () => {
+  return <LoginLayout />;
+};
+
+export default login;
