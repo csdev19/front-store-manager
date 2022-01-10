a6ceb0edf5454c32764b120df3541fbc2009651e
cristiandev <cristiansotomayor.dev@gmail.com>
2022-01-09 21:12:08 -0500

    product layout component

---
+++ modules/inventory/products/ProductsLayout.tsx
@@ -0,0 +1,109 @@
+import {
+  Button,
+  Container,
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  ThemeProvider,
+} from '@mui/material';
+import React, { VoidFunctionComponent } from 'react';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import { IProduct } from 'core/interfaces/products.interface';
+import AddIcon from '@mui/icons-material/Add';
+import AdminLayout from 'components/AdminLayout';
+import theme from 'theme';
+
+function createData(
+  id: number,
+  name: string,
+  description: string,
+  frontImage: string,
+  images: string[],
+  price: number,
+  stock: number,
+  isVisible: boolean,
+): IProduct {
+  return {
+    id,
+    name,
+    description,
+    frontImage,
+    images,
+    price,
+    stock,
+    isVisible,
+  };
+}
+
+const rows = [
+  createData(1, 'Producto 1', 'Descripcion producto 1', '', [], 159, 10, true),
+  createData(2, 'Producto 2', 'Descripcion producto 2', '', [], 237, 10, true),
+  createData(3, 'Producto 3', 'Descripcion producto 3', '', [], 262, 10, true),
+  createData(4, 'Producto 4', 'Descripcion producto 4', '', [], 305, 10, true),
+  createData(5, 'Producto 5', 'Descripcion producto 5', '', [], 356, 10, true),
+];
+
+const ProductsLayout: VoidFunctionComponent = () => {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <AdminLayout />
+        <Container maxWidth="lg">
+          <h1>Products layout</h1>
+
+          <div>
+            <Button variant="contained" startIcon={<AddIcon />}>
+              Crear
+            </Button>
+          </div>
+
+          <div>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Nombre</TableCell>
+                    <TableCell>Precio</TableCell>
+                    <TableCell>Inventario</TableCell>
+                    <TableCell align="center">Acciones</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell>{row.price}</TableCell>
+                      <TableCell>{row.stock}</TableCell>
+                      <TableCell align="center">
+                        <IconButton aria-label="delete">
+                          <EditIcon />
+                        </IconButton>
+
+                        <IconButton aria-label="delete">
+                          <DeleteIcon />
+                        </IconButton>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+        </Container>
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default ProductsLayout;
